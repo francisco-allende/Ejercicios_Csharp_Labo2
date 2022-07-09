30c965f0971ba0aa7c41fb42f89fc85a6487a4a7
“francisco” <franallende2000@gmail.com>
2022-07-09 14:32:52 -0300

    poli

---
+++ Centralita/2. Polimorfismo/Biblio/Centralita.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblio
+{
+    public class Centralita
+    {
+        private List<Llamada> listaDeLlamadas;
+        protected string razonSocial;
+
+        public float GananciasPorLocal { get => this.CalcularGanancia(Llamada.TipoLlamada.Local); }
+        public float GananciasPorProvincial { get => this.CalcularGanancia(Llamada.TipoLlamada.Provincial); }
+        public float GananciasPorTotal { get => this.CalcularGanancia(Llamada.TipoLlamada.Todas); }
+        public List<Llamada> Llamadas { get => this.listaDeLlamadas; }
+
+        private float CalcularGanancia(Llamada.TipoLlamada tipo)
+        {
+            float ganancia = 0;
+
+            foreach (Llamada item in this.Llamadas)
+            {
+                if (tipo is Llamada.TipoLlamada.Provincial && item is Provincial)
+                {
+                    ganancia += ((Provincial)item).CostoLlamada;
+                }
+                else if (tipo is Llamada.TipoLlamada.Local && item is Local)
+                {
+                    ganancia += ((Local)item).CostoLlamada;
+                }
+                else if (tipo is Llamada.TipoLlamada.Todas)
+                {
+                    switch (item)
+                    {
+                        case Local:
+                            ganancia += ((Local)item).CostoLlamada;
+                            break;
+
+                        case Provincial:
+                            ganancia += ((Provincial)item).CostoLlamada;
+                            break;
+                    }
+                }
+            }
+
+            return ganancia;
+        }
+
+        public Centralita()
+        {
+            this.listaDeLlamadas = new List<Llamada>();
+        }
+
+        public Centralita(string nombreEmpresa)
+            :this()
+        {
+            this.razonSocial = nombreEmpresa;
+        }
+
+        public void OrdenarLlamadas()
+        {
+            this.Llamadas.Sort(Llamada.OrdenarPorDuracion);
+        }
+
+        private string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append($"Razon social: {this.razonSocial}\n");
+            sb.Append($"Total Ganancias Local: {this.GananciasPorLocal}\n");
+            sb.Append($"Total Ganancias Provincial: {this.GananciasPorProvincial}\n");
+            sb.Append($"Total Ganancia: {this.GananciasPorTotal}\n");
+
+            sb.Append("\n******     Detalle llamados    ******\n\n");
+            foreach (Llamada item in this.Llamadas)
+            {
+                sb.Append($"{item.ToString()}\n");
+            }
+
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return this.Mostrar();
+        }
+
+        private void AgregarLlamada(Llamada nuevaLlamada)
+        {
+            this.listaDeLlamadas.Add(nuevaLlamada);
+        }
+
+        public static Centralita operator +(Centralita c1, Llamada l1)
+        {
+            if(c1 != l1)
+            {
+                c1.AgregarLlamada(l1);
+            }
+
+            return c1;
+        }
+
+        public static bool operator ==(Centralita c1, Llamada l1)
+        {
+            foreach (Llamada item in c1.listaDeLlamadas)
+            {
+                if(l1 == item)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(Centralita c1, Llamada l1)
+        {
+            return !(c1 == l1);
+        }
+    }
+}
---
+++ Centralita/2. Polimorfismo/Biblio/Provincial.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblio
+{
+    public class Provincial : Llamada
+    {
+        public enum Franja{Franja_1, Franja_2, Franja_3}
+        private Franja franjaHoraria;
+
+        public override float CostoLlamada { get => this.CalcularCosto(); }
+
+        private float CalcularCosto()
+        {
+            if (this.franjaHoraria == Franja.Franja_1)
+            {
+                return this.duracion * 0.99f;
+            }
+
+            if (this.franjaHoraria == Franja.Franja_2)
+            {
+                return this.duracion * 1.25f;
+            }
+
+            if (this.franjaHoraria == Franja.Franja_3)
+            {
+                return this.duracion * 0.66f;
+            }
+
+            return 0;
+        }
+
+        public Provincial(Franja franjaHoraria, Llamada llamada)
+            :this(llamada.NroOrigen, franjaHoraria, llamada.Duracion, llamada.NroDestino)
+        {
+
+        }
+
+        public Provincial(string origen, Franja franjaHoraria, float duracion, string destino)
+            : base(duracion, destino, origen)
+        {
+            this.franjaHoraria = franjaHoraria;
+        }
+
+        protected new string Mostrar()
+        {
+            return $"{base.Mostrar()} - {this.CostoLlamada} - {this.franjaHoraria}";
+        }
+
+        public override string ToString()
+        {
+            return this.Mostrar();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Provincial;
+        }
+
+    }
+}

23494f70083c06876b81b29d23f9079330f07ef5
“francisco” <franallende2000@gmail.com>
2022-07-27 20:29:37 -0300

    muy buen y simple ejemplo

---
+++ Hilos/Relojero/WinFormsApp/WinFormsApp/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace WinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.labelHora = new System.Windows.Forms.Label();
+            this.btn_iniciar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(12, 55);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(582, 232);
+            this.richTextBox.TabIndex = 0;
+            this.richTextBox.Text = "";
+            // 
+            // labelHora
+            // 
+            this.labelHora.AutoSize = true;
+            this.labelHora.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelHora.Location = new System.Drawing.Point(12, 21);
+            this.labelHora.Name = "labelHora";
+            this.labelHora.Size = new System.Drawing.Size(44, 21);
+            this.labelHora.TabIndex = 1;
+            this.labelHora.Text = "Hora";
+            // 
+            // btn_iniciar
+            // 
+            this.btn_iniciar.Location = new System.Drawing.Point(519, 21);
+            this.btn_iniciar.Name = "btn_iniciar";
+            this.btn_iniciar.Size = new System.Drawing.Size(75, 23);
+            this.btn_iniciar.TabIndex = 2;
+            this.btn_iniciar.Text = "Iniciar";
+            this.btn_iniciar.UseVisualStyleBackColor = true;
+            this.btn_iniciar.Click += new System.EventHandler(this.btn_iniciar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 304);
+            this.Controls.Add(this.btn_iniciar);
+            this.Controls.Add(this.labelHora);
+            this.Controls.Add(this.richTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Label labelHora;
+        private System.Windows.Forms.Button btn_iniciar;
+    }
+}
+
---
+++ Hilos/Relojero/WinFormsApp/WinFormsApp/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+       
+        }
+
+        private async void btn_iniciar_Click(object sender, EventArgs e)
+        {
+            //Loop, hasta que se cierre la app
+            for(; ; )
+            {
+                string time = await AsignarHora(); //retorna string en realidad. Debe ser task<stirng> para el await
+                labelHora.Text = time; //modifico desde aca ya que este control es del hilo 1
+                richTextBox.Text += "\n"+time;
+            }
+        }
+
+        //retorna string. Para esto, necesito guardarlo en un Task<stirng>
+        //ya que el int normal, void, string no pueden tener await
+        private Task<string> AsignarHora()
+        {   
+            Task<string> getTime = Task.Run(() =>
+            {
+                string time = "";
+                DateTime dt = DateTime.Now;
+                Thread.Sleep(1000);
+                DateTime dt2 = DateTime.Now;
+
+                if (dt.Second != dt2.Second)
+                {
+                    time = $"{DateTime.Now:G}";
+                }
+                return time;
+            });
+
+            return getTime;
+        }
+
+    }
+}

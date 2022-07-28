975caa07790b9783d8cb9b11e09bee01c429ee6a
“francisco” <franallende2000@gmail.com>
2022-07-27 23:52:19 -0300

    tasks y eventos

---
+++ Eventos/Relojero_II/WinFormsApp/WinFormsApp/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        private Temporizador temporizador;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.temporizador = new Temporizador(1000);
+            temporizador.TiempoCumplido += AsignarHora;
+        }
+
+        private void btn_iniciar_Click(object sender, EventArgs e)
+        {
+            temporizador.IniciarTemporizador(); 
+        }
+
+        private void btn_Finalizar_Click(object sender, EventArgs e)
+        {
+            temporizador.DetenerTiempo();
+        }
+
+        private void AsignarHora()
+        {   
+            //Invoke required sera true si estamos en un hilo distinto al que instanciamos
+            //el form y los controles
+            //false si no estamos en ese hilo
+            if(labelHora.InvokeRequired) 
+            {
+                //Guardo en un delegado el metodo que quiero llamar
+                //desde el hilo donde se instancio el control o form. 
+                //El metodo que va a cambiar el control. Al ser este, es recursivo
+                //Con invoke ejecuto el metodo referenciado desde el hilo correcto
+                Action asignarHora = AsignarHora;
+                labelHora.Invoke(asignarHora);
+            }
+            else
+            {
+                //Recursivo, vuelvo al mismo metodo desde el primer hilo.
+                //El invokeRequired pasa a ser false y ya puedo cambiar el label
+                labelHora.Text = $"{DateTime.Now:G}";
+            }
+        }
+
+      
+    }
+}
---
+++ Eventos/Relojero_II/WinFormsApp/WinFormsApp/Temporizador.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WinFormsApp
+{
+    public delegate void DelegadoTemporizador();
+
+    public class Temporizador
+    {
+        public event DelegadoTemporizador TiempoCumplido;
+
+        private CancellationToken cancellationToken;
+        private CancellationTokenSource cancellationTokenSource;
+        private Task hilo;
+        private int intervalo;
+
+        //Como no instancie la task aun, pongo el is not null asi no se rompe todo
+        public bool EstaActivo
+        { 
+            get
+            {
+                return hilo is not null && hilo.Status == TaskStatus.Running;
+            }
+        }
+        public int Intervalo { get => intervalo; set => intervalo = value; }
+
+        public Temporizador(int intervalo)
+        {
+            this.Intervalo = intervalo;
+        }
+
+        public void IniciarTemporizador()
+        {
+            if (!EstaActivo)
+            {
+                //asigno los cancel token
+                cancellationTokenSource = new CancellationTokenSource();
+                cancellationToken = cancellationTokenSource.Token;
+
+                //instancio la task con una funcion que retonre void y activo con strat
+                hilo = new Task(CorrerTiempo);
+                hilo.Start();
+            }
+        }
+
+        private void CorrerTiempo()
+        {
+            //While el evento tenga un metodo suscripto y no me hayan pedido cancelar el hilo
+            while(TiempoCumplido != null &&
+                !cancellationToken.IsCancellationRequested)
+            {
+                TiempoCumplido.Invoke();
+                Thread.Sleep(intervalo);
+            }
+        }
+
+        public void DetenerTiempo()
+        {
+            if(EstaActivo)
+            {
+                cancellationTokenSource.Cancel();
+            }
+        }
+    }
+}
